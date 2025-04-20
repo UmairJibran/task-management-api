5d68ac3df40afcfe67f20f6d9ed1c7e33474e79e
Umair Jibran <me@umairjibran.com>
2025-04-21 02:01:53 +0500

    create sb client

---
+++ src/config/supabase.client.js
@@ -0,0 +1,9 @@
+const { createClient } = require('@supabase/supabase-js');
+require('dotenv').config();
+
+const supabase = createClient(
+  process.env.FA_SUPABASE_URL,
+  process.env.FA_SUPABASE_PUBLIC_API_KEY
+);
+
+module.exports = supabase;
